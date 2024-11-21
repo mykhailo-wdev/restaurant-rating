83ed49885868d244d29e360149fd12f55bbaf77b
Mike Mike <mike_insider@proton.me>
2024-11-21 21:08:31 +0200

    Added the restaurant page logic

--- components/restaurant/Row.vue
+++ components/restaurant/Row.vue
@@ -1,20 +1,40 @@
 <template>
-    <div class="row">
-        <h4 class="header rank">1</h4>
-        <a href="/" class="header link">McDonalds</a>
+    <div class="row" v-if="isHeader">
+        <h4 class="header">Rank</h4>
+        <h4 class="header">Chain</h4>
+    </div>
+    
+    <div v-else class="row" :class="{'highlight-row': isEven(index)}">
+        <h4 class="header rank">{{ rank }}</h4>
+        <a :href="`/restaurants/${name}`" class="header link">{{ name }}</a>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+interface RowProps {
+    rank?: number;    
+    name?: string;   
+    index?: number;   
+    isHeader?: boolean; 
+}
 
+
+const props = defineProps<RowProps>()
+
+
+const isEven = (index: number = 0): boolean => {
+  return index % 2 === 0;
+}
 </script>
 
 <style scoped>
+
 .row {
   border-top: 1px solid rgba(128, 128, 128, 0.336);
   display: flex;
   align-items: center;
 }
+
 .header {
   font-size: 1.25rem;
   color: rgb(10, 31, 148);
@@ -24,13 +44,20 @@
   font-weight: 700;
   width: 4rem;
 }
+
+
 .rank {
   color: black;
 }
+
 .link {
   text-decoration: none;
   color: rgb(228, 157, 27);
   width: auto;
   font-weight: 500;
+}
+
+.highlight-row {
+  background-color: rgba(128, 128, 128, 0.15);
 }
 </style>
--- components/restaurant/Table.vue
+++ components/restaurant/Table.vue
@@ -3,15 +3,23 @@
         <h1>TOP 50: THE RANKING</h1>
         <div class="table-container">
             <div class="table-col">
+                <RestaurantRow :isHeader="true" />
                 <RestaurantRow 
-                    v-for="restaurant in restaurantsOrganized.first" 
+                    v-for="(restaurant, index) in restaurantsOrganized.first" 
                     :key="restaurant.id"
+                    :name="restaurant.name"
+                    :rank="restaurant.rank"
+                    :index="index"
                 />
             </div>
             <div class="table-col">
+                <RestaurantRow :isHeader="true" />
                 <RestaurantRow 
-                    v-for="restaurant in restaurantsOrganized.second" 
+                    v-for="(restaurant, index) in restaurantsOrganized.second" 
                     :key="restaurant.id"
+                    :name="restaurant.name"
+                    :rank="restaurant.rank"
+                    :index="index"
                 />
             </div>
         </div>
@@ -21,8 +29,8 @@
 <script setup lang="ts">
 import restaurants from '@/data.json'
 const restaurantsOrganized = {
-    first: [...restaurants].splice(0, 25),
-    second: [...restaurants].splice(0, 25)
+    first: [...restaurants].slice(0, 25),
+    second: [...restaurants].slice(25)
 }
 </script>
 
