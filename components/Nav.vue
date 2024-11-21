aed187150a994d29fe6755fa2d495a09e020395f
Mike Mike <mike_insider@proton.me>
2024-11-21 19:45:51 +0200

    Created navbar

---
+++ components/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Restaurantly</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/restaurants">Restaurants</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.ml-auto {
+    margin-left: auto;
+}
+</style>
